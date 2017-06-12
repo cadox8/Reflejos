4ba82b5b1d0bee6ffc76ee98d0645f6dd8173d65
Iván <cadox8@gmail.com>
2017-06-12 12:12:25 +0200

    Fixed game

--- reflejos.ino
+++ reflejos.ino
@@ -6,10 +6,10 @@ byte pul4 = 4;
 byte red = 11; //Player 2
 byte pul12 = 12;
 
-String winner;
-boolean gameOver;
+String winner = "Espera, ¿nadie ha ganado? ¿Tan mancos sois? Peor que WillyRex en el COD, eh";
+byte gameOver = 0;
 
-int csgo; //Ja jaaaaaaaa CounterStrike:GO
+int csgo = 0; //Ja jaaaaaaaa CounterStrike:GO
 
 void setup() {
   Serial.begin(9600); //Port? Are we boats now?
@@ -20,47 +20,40 @@ void setup() {
   
   pinMode(pul4, INPUT);
   pinMode(pul12, INPUT);
-
-  gameOver = false;
-  getReck(5); //We want default message :D
-  csgo = 0;
 }
 
 void loop() {
-  if (!gameOver) {
+  counterStrike();
+  if (gameOver == 0) {
     Serial.println("COMIENZA EL JUEGO CUANDO SE APAGUE EL LED BLANCO");
     digitalWrite(white, HIGH);
     delay(random(11) * 1000); //Set to 11 to get from 0 to 10
     digitalWrite(white, LOW); 
+    delay(500);
   }
-
-  switch(digitalRead(pul4)) {
-    case HIGH:
-      if (gameOver) break;
+ 
+  if (digitalRead(pul4) == 1 && gameOver == 0) {
       digitalWrite(green, HIGH);
       getReck(1);
-      break;
-  }
-  switch(digitalRead(pul12)) {
-    case HIGH:
-      if (gameOver) break;
-      digitalWrite(green, HIGH);
-      getReck(2);
-      break;
   }
 
-  if (!gameOver) return;
-  Serial.println("HAS GANADO " + winner + " en " + csgo / 1000 + " segundos");
-  Serial.println("PULSA RESET PARA UN NUEVO JUEGO");
+  if (digitalRead(pul12) == 1 && gameOver == 0) {
+      digitalWrite(red, HIGH);
+      getReck(2);
+  }
+
+  if (gameOver == 0){
+    Serial.println("Nadie ha ganado, ha empezado de nuevo la ronda");
+  }
 }
 
 void counterStrike(){
-  if (gameOver) return;
+  if (gameOver == 1) return;
   csgo++;
 }
 
 void getReck(int player){
-  gameOver = true;
+  gameOver = 1;
 
   switch(player) {
       case 1:
@@ -69,9 +62,7 @@ void getReck(int player){
       case 2:
         winner = "ROJO";
         break;
-      default:
-        winner = "Espera, ¿nadie ha ganado? ¿Tan mancos sois? Peor que WillyRex en el COD, eh";
-        break;
   }
+  Serial.println("HAS GANADO " + winner + " EN " + csgo + " SEGUNDOS");
+  Serial.println("PULSA RESET PARA UN NUEVO JUEGO");
 }
-
